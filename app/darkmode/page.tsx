b7cce518577078ba9c28a32e26d4c3c3b157336a
kazuya-akehi <xakehi@booklista.co.jp>
2025-07-06 08:12:06 +0900

    dark mode

---
+++ app/darkmode/page.tsx
@@ -0,0 +1,10 @@
+export default function PostsPage() {
+  return (
+    <main>
+      <h1>darkMode</h1>
+      <div className="text-black bg-white dark:bg-gray-900 dark:text-white p-4 rounded">
+        <p>ダークモード対応のボックス</p>
+      </div>
+    </main>
+  );
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import ThemeToggle from "@/components/ThemeToggle";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,10 +24,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="ja">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <header className="p-4">
+          <ThemeToggle />
+        </header>
         {children}
       </body>
     </html>
