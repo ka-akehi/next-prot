bd2893a1fd13a590a14530aecd2030e68e9ff26b
kazuya-akehi <xakehi@booklista.co.jp>
2025-08-03 11:13:38 +0900

    add

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,9 @@
-const { createDefaultPreset } = require('ts-jest');
-
-const tsJestTransformCfg = createDefaultPreset().transform;
-
 /** @type {import("jest").Config} **/
 module.exports = {
-  testEnvironment: 'node',
-  transform: {
-    ...tsJestTransformCfg,
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/$1',
   },
-  scripts: {
-    test: 'jest',
-  },
+  setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
 };
