cdc2ae4d5f3ecd0bf6d52d6e9476cb8703b9c40b
kazuya-akehi <xakehi@booklista.co.jp>
2025-08-07 00:53:38 +0900

    add

--- jest.config.ts
+++ jest.config.ts
@@ -3,11 +3,14 @@ import type { Config } from 'jest';
 const config: Config = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/$1',
   },
-  testPathIgnorePatterns: ['/node_modules/', '/.next/'],
+  transformIgnorePatterns: ['node_modules/(?!(next-auth)/)'],
 };
 
 export default config;
